86fe26f7ff8af1f2a70d939abc632a8cd75b68ae
Naveen-Jangir-1984 <65364442+Naveen-Jangir-1984@users.noreply.github.com>
2020-05-17 16:16:44 +0530

    Update calculator_steps.py

--- features/steps/calculator_steps.py
+++ features/steps/calculator_steps.py
@@ -1,8 +1,8 @@
 from zeep import Client
 from behave import given, when, then
 
-wsdl = 'http://www.dneonline.com/calculator.asmx?wsdl'
-client = Client(wsdl=wsdl)
+# wsdl = 'http://www.dneonline.com/calculator.asmx?wsdl'
+# client = Client(wsdl=wsdl)
 
 @given('a number {number1:d} is entered')
 def step_impl(context, number1):
@@ -14,11 +14,13 @@ def step_impl(context, number2):
 
 @when('add button is pressed')
 def step_impl(context):
-    context.result = client.service.Add(context.number1, context.number2)
+#     context.result = client.service.Add(context.number1, context.number2)    
+    context.result = context.number1 + context.number2
 
 @when('subtract button is pressed')
 def step_impl(context):
-    context.result = client.service.Subtract(context.number1, context.number2)
+#     context.result = client.service.Subtract(context.number1, context.number2)    
+    context.result = context.number1 - context.number2
 
 @then('result is {result:d}')
 def step_impl(context, result):
